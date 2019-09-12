3fa649c3a8d1ed35cdab095520c783a6e64d84f6
akash.srivastava <akash.srivastava@PUN-ASRIVAST01>
2019-09-12 13:45:51 +0530

    upadated sql server icon

--- profile/src/app/data/skills.ts
+++ profile/src/app/data/skills.ts
@@ -2,7 +2,7 @@ export const skills: string[] = [
     "https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png",
     "http://pluspng.com/img-png/logo-javascript-png-javascript-tutorials-400.png",
     "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png",
-    "https://compassconsult.com/wp-content/uploads/2016/01/SQL-Server-logo.png",
+    "https://www.freeiconspng.com/uploads/sql-server-icon-png-2.png",
     "https://cdn.iconscout.com/icon/free/png-128/html5-10-569380.png",
     "http://www.myiconfinder.com/uploads/iconsets/256-256-8b61de4c84033266e15317a6eb9fda2d-css3.png",
     "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
