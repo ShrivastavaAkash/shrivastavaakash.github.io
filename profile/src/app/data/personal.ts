cbaf6db0d6aae38cf3e0543307d7e66ec1c22418
akash.srivastava <akash.srivastava@PUN-ASRIVAST01>
2019-09-21 21:21:41 +0530

    SEO tags added

--- profile/src/app/data/personal.ts
+++ profile/src/app/data/personal.ts
@@ -2,5 +2,5 @@ export const personalData = {
     name: "Akash Shrivastava",
     email: "shrivastavaakash3894@gmail.com",
     role: "___ Developer.",
-    profileImage: "https://i.stack.imgur.com/3IHqD.jpg"
+    profileImage: "/assets/images/Akash_shrivastava.jpg"
 }
