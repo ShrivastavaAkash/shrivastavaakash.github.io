e25176b1e79046fcd0180f67a2d8a8a6b69cef7c
akash.srivastava <akash.srivastava@PUN-ASRIVAST01>
2019-12-24 19:56:19 +0530

    changed links to github

--- profile/src/app/data/personal.ts
+++ profile/src/app/data/personal.ts
@@ -2,7 +2,7 @@ export const personalData = {
     name: "Akash Shrivastava",
     email: "shrivastavaakash3894@gmail.com",
     role: "___ Developer.",
-    profileImage: "https://shrivastavaakash.github.io/assets/images/Akash_shrivastava.jpg",
+    profileImage: ("https://" + window.location.host) + "/assets/images/Akash_shrivastava.jpg",
     description: "Personal profile, blog and contact details.",
-    website: "https://shrivastavaakash.github.io"
+    website: ("https://" + window.location.host)
 }
