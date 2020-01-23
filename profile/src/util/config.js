523b6d80ae06c5fb0ffaeb81edb889ab5e445673
akash.srivastava <akash.srivastava@PUN-ASRIVAST01>
2020-01-23 22:46:17 +0530

    added showdown

--- profile/src/util/config.js
+++ profile/src/util/config.js
@@ -1,14 +1,18 @@
 module.exports = {
-    parentDirectoryName: "../../../blog",
-    filename: "index.html",
-    metaDataTSSource: "../app/data/blog-meta.ts",
-    metaDataDirectoryName: "data",
-    metaDataFileName: "metaData.js",
-    templatePath: "./template/helmet.html",
-    placeholderInTemplate: "<!--???PLACEHOLDER???-->",
-    distPath: "../../dist/",
-    rootFolder: "../../../",
-    get metaDataFilePath () {
-        return "./" + this.metaDataDirectoryName + "/" + this.metaDataFileName
-    }
-}
+  parentDirectoryName: "../../../blog",
+  filename: "index.html",
+  metaDataTSSource: "../app/data/blog-meta.ts",
+  metaDataDirectoryName: "data",
+  metaDataFileName: "metaData.js",
+  templatePath: "./template/helmet.html",
+  placeholderInTemplate: {
+    header: "<!--???PLACEHOLDER_HEADERS???-->",
+    content: "<!--???PLACEHOLDER_CONTENT???-->"
+  },
+  markdownPath: "../assets/markdowns/",
+  distPath: "../../dist/",
+  rootFolder: "../../../",
+  get metaDataFilePath() {
+    return "./" + this.metaDataDirectoryName + "/" + this.metaDataFileName;
+  }
+};
