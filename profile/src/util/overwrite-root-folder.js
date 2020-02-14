cc95492369123996a2ac13c4ebf5cfe85fff138a
akash.srivastava <akash.srivastava@PUN-ASRIVAST01>
2020-02-14 16:00:23 +0530

    fixed build structure

--- profile/src/util/overwrite-root-folder.js
+++ profile/src/util/overwrite-root-folder.js
@@ -1,12 +1,31 @@
 const fs = require("fs-extra");
 const config = require("./config");
+const toDelete = /main-.*.js|index.html|assets|favicon.ico|styles.*.css|polyfills-.*.js|runtime-.*.js|scripts.*.js|3rdpartylicenses.txt/g;
 
-if (fs.existsSync(config.distPath)){ 
-    console.log(config.distPath + " folder exists!");
-    fs.copy(config.distPath, config.rootFolder, function (err) {
-        if (err) return console.error(err)
-        console.log('overwrite success!')
-    });
+// remove existing
+if (fs.existsSync(config.rootFolder)) {
+  let files = fs.readdirSync(config.rootFolder);
+
+  if (files && files.length > 0)
+    files
+      .filter(v => v.match(toDelete))
+      .forEach(v => {
+        if (fs.existsSync(config.rootFolder + "/" + v)) {
+          fs.remove(config.rootFolder + "/" + v);
+          console.log(`removing ${config.rootFolder + "/" + v}...!`);
+        }
+      });
+}
+
+console.log("\nClean up finished\n\nStarting copying new build...!\n");
+
+// add new
+if (fs.existsSync(config.distPath)) {
+  console.log("target folder - " + config.distPath + " exists!");
+  fs.copy(config.distPath, config.rootFolder, function(err) {
+    if (err) return console.error(err);
+    console.log("overwrite success!");
+  });
 } else {
-    console.log(config.distPath + " folder does not exists!");
+  console.log(config.distPath + " folder does not exists!");
 }
