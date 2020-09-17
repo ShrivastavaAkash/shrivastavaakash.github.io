f042cfcfea9d7d9f92c88f5c6947250b09f45fe5
akash.srivastava <akash.srivastava@PUN-ASRIVAST01>
2020-09-18 02:05:12 +0530

    changed profile picture ref

--- profile/src/app/data/personal.ts
+++ profile/src/app/data/personal.ts
@@ -2,7 +2,7 @@ export const personalData = {
     name: "Akash Shrivastava",
     email: "shrivastavaakash3894@gmail.com",
     role: "___ Developer.",
-    profileImage: "/assets/images/Akash_shrivastava.jpg",
+    profileImage: "https://avatars0.githubusercontent.com/u/12945858",
     description: "Personal profile, blog and contact details.",
     website: ("https://" + window.location.host),
     themeColor: "#222222",
